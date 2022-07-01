9e55a53eba75c5dca509369844f29880ec133e5d
kashvi <kashvi.dwarka23@gmail.com>
2022-06-30 18:47:50 -0700

    added typing feature to name and removed the extra section on page

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ function toggleMenu(){
     menuToggle.classList.toggle('active');
     
 }
+
+/* typewriter effect*/
+var quoteArray = ["Hi, I'm Kashvi Dwarka"];
+var textPosition = 0; 
+var speed = 110;
+
+function typewriter() {
+    document.querySelector(".name-with-effect").innerHTML = quoteArray[0].substring(0, textPosition) + '<span>|</span>'; /* or \u25AE */
+  
+    if(textPosition++ != quoteArray[0].length)
+      setTimeout(typewriter, speed);
+  }
+  
+  window.addEventListener("load", typewriter);
+  window.removeEventListener("span");
